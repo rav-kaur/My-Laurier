0f6e307c928109e2ba723b603b706aff3818ec59
Ravneet Kaur <kaur9100@mylaurier.ca>
2019-11-10 16:33:35 -0500

    Login

---
+++ app/src/main/java/com/example/mylaurier/LoginActivity.java
@@ -0,0 +1,70 @@
+package com.example.mylaurier;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+//import android.content.Intent;
+import androidx.annotation.NonNull;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        button = findViewById(R.id.login);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == button){
+                    LoginUser();
+                }
+            }
+        });
+    }
+    public void LoginUser(){
+        String Email = email.getText().toString().trim();
+        String Password = password.getText().toString().trim();
+        mAuth.signInWithEmailAndPassword(Email, Password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            currentUser = mAuth.getCurrentUser();
+                            finish();
+                            //startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+                            Toast.makeText(LoginActivity.this, "logged in",
+                                    Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(LoginActivity.this, "couldn't login",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
