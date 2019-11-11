3d3c0bcd480f3c0dfc32b8b258de4f0e5b60c0d9
Nicholas Chu <chu.Nicholas@hotmail.com>
2019-11-11 08:48:00 -0500

    added user location tracking. reduced point size on the layer. app will ask for user permission on startup if it does not have it already

--- app/src/main/java/com/example/mylaurier/MapsActivity.java
+++ app/src/main/java/com/example/mylaurier/MapsActivity.java
@@ -1,26 +1,46 @@
 package com.example.mylaurier;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toolbar;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.*;
+import com.google.maps.android.data.kml.KmlContainer;
+import com.google.maps.android.data.kml.KmlGroundOverlay;
 import com.google.maps.android.data.kml.KmlLayer;
+import com.google.maps.android.data.kml.KmlPlacemark;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final int DEFAULT_ZOOM = 15;
     private GoogleMap mMap;
     private static final String TAG = "myMap";
+    private FusedLocationProviderClient mFusedLocationProviderClient = null;
+    private Location mLastKnownLocation = null;
+    private boolean mLocationPermissionGranted = false;
+    private LatLng laurierWaterloo = new LatLng(43.4723571, -80.5285286);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +50,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Construct a FusedLocationProviderClient.
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
     }
 
 
@@ -46,12 +70,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng laurierWaterloo = new LatLng(43.4723571, -80.5285286);
-        //mMap.addMarker(new MarkerOptions().position(laurierWaterloo).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(laurierWaterloo));
         // change camera zoom level, without changing position
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(DEFAULT_ZOOM));
+
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI();
+
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation();
+
         // add custom map layer
         KmlLayer layer;
         try {
@@ -59,20 +87,112 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             layer.addLayerToMap();
         }
         catch (Exception e){
-
+            Log.d(TAG, "could not load KML Layer");
         }
-        checkLocationPermissions();
+        // doesn't work. android only supports one KML file at a time?
+//        KmlLayer buildings, foodstores;
+//        try {
+//            buildings = new KmlLayer(mMap, R.raw.buildings, getApplicationContext());
+//            foodstores = new KmlLayer(mMap, R.raw.foodstores, getApplicationContext());
+//            foodstores.
+//            //buildings.addLayerToMap();
+//            //foodstores.addLayerToMap();
+//        }
+//        catch (Exception e) {
+//            Log.d(TAG, "could not load KML Layer");
+//        }
     }
 
     /*
     check user permissions, this should probably be in main activity
      */
-    public void checkLocationPermissions() {
+    private boolean checkLocationPermissions() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // permission not granted
-            Log.d(TAG, "Permission for Location not granted, unable to locate user");
-            Snackbar mySnackbar = Snackbar.make(getWindow().getDecorView().getRootView(),"Unable to determine location due to location permissions.", 10);
+            Log.d(TAG, "App does not have location permissions");
+            final Activity temp = this;
+            // inner class for snackbar button
+            class ChangeLocationListener implements View.OnClickListener {
+                @Override
+                public void onClick(View v) {
+                    ActivityCompat.requestPermissions(temp,
+                            new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+                }
+            }
+            Snackbar mySnackbar = Snackbar.make(this.getWindow().getDecorView().findViewById(android.R.id.content), "Allow app to access location?", 10);
+            mySnackbar.setAction("Allow", new ChangeLocationListener());
             mySnackbar.show();
+            return false;
+        }
+        return true;
+    }
+
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (mLocationPermissionGranted) {
+                Task locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            mLastKnownLocation = (Location)task.getResult();
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(mLastKnownLocation.getLatitude(),
+                                            mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            // default location
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(laurierWaterloo, 15));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch(SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+        updateLocationUI();
+    }
+
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+            if (mLocationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                mLastKnownLocation = null;
+                checkLocationPermissions();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
         }
     }
 }
